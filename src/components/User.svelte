10ab8d2df8e9d46e5e01bf57755b6b078b448e4c
GrauzeGustavs <78407343+GrauzeGustavs@users.noreply.github.com>
2022-11-29 12:34:56 +0000

    Stuff

---
+++ src/components/User.svelte
@@ -0,0 +1,34 @@
+<script>
+import PocketBase from 'pocketbase';
+
+const pb = new PocketBase('https://db.0xff.lv');
+
+let email = ""
+
+let password = ""
+
+let username = ""
+
+async function login(){
+    const authData = await pb.collection('lietotaji').authWithPassword(
+    email,
+    password,
+);
+
+// after the above you can also access the auth data from the authStore
+console.log(pb.authStore.isValid);
+console.log(pb.authStore.token);
+console.log(pb.authStore.model.id);
+
+username = authData.record.username
+}
+</script>
+
+
+
+<div>
+    <h1>Tavs vards ir: {username}</h1>
+    <input type="text" name="" id="" bind:value={email}>
+    <input type="text" name="" id="" bind:value={password}>
+    <button on:click={login}>pierakstites</button>
+</div>
