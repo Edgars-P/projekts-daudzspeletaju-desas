042847478c4f1be6b15b7528e5738587b869d5a5
Agnis800 <78017711+Agnis800@users.noreply.github.com>
2022-12-01 11:44:37 +0000

    added login form

---
+++ src/components/LoginPrompt.svelte
@@ -0,0 +1,9 @@
+<div>
+    <form>
+        <label for="user">User:</label><br>
+        <input type="text" id="user" name="user"><br>
+        <label for="pwd">Password:</label><br>
+        <input type="password" id="pwd" name="pwd"><br>
+        <input type="submit" value="Submit">
+    </form>
+</div>
