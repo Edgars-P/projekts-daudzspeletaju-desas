5a4b680f5cecbcfd90ea6bc73a69b00d73b53460
Edgars <01dtepolis@rvt.lv>
2022-12-03 19:52:15 +0200

    Labāks login

--- src/components/LoginPrompt.svelte
+++ src/components/LoginPrompt.svelte
@@ -1,9 +1,52 @@
-<div>
-    <form>
-        <label for="user">User:</label><br>
-        <input type="text" id="user" name="user"><br>
-        <label for="pwd">Password:</label><br>
-        <input type="password" id="pwd" name="pwd"><br>
-        <input type="submit" value="Submit">
-    </form>
-</div>
+<script>
+    export let isOpen = false;
+
+    let username = "";
+    let password = "";
+
+    function handleLogin() {
+        // db.login(username, password);
+    }
+
+    function handleClose(event) {
+        // Aizver popup
+    }
+</script>
+
+{#if isOpen}
+    <!-- svelte-ignore a11y-click-events-have-key-events -->
+    <div class="overlay" on:click={handleClose}>
+        <div class="modal">
+            <form on:submit|preventDefault={handleLogin}>
+                <label for="username">Username:</label>
+                <input type="text" bind:value={username} id="username" />
+                <label for="password">Password:</label>
+                <input type="password" bind:value={password} id="password" />
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    </div>
+{/if}
+
+<style>
+    .overlay {
+        position: fixed;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        background-color: rgba(0, 0, 0, 0.5);
+        z-index: 1;
+    }
+
+    .modal {
+        position: fixed;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        background-color: white;
+        padding: 1em;
+        border-radius: 0.5em;
+        z-index: 2;
+    }
+</style>
