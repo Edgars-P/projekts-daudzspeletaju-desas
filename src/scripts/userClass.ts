fd6bf88c6077c9ec8583d26ae530721fab0a0d60
Agnis800 <78017711+Agnis800@users.noreply.github.com>
2023-01-30 07:06:20 +0000

    Pievienoti komentāri

--- src/scripts/userClass.ts
+++ src/scripts/userClass.ts
@@ -7,14 +7,19 @@ export default class User{
         this.id = id;
     }
     async getName() {
+        // ja vārds ir saglabāts, tad atgriež lietotājvārdu
         if (this.name) {return this.name}
+        // atrod vāŗdu pēc lietotāja ID (nākamās 4 rindas)
         let user = await pb
             .collection("lietotaji")
             .getOne(this.id, { $autoCancel: false })
             .catch(logout)
+        // ja lietotājs neeksistē, tad tas atgriež false
         if (!user)
             return false
+        // saglabā lietotājvārdu nākamajai reizei
         this.name = user.username
+        // atgriež lietotājvārdu
         return user.username
     
     }
