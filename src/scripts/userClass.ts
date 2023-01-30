260ec6c45d38502134b7cd34c69834ecc377dd6b
Agnis800 <78017711+Agnis800@users.noreply.github.com>
2023-01-30 08:48:27 +0000

    Izlabotas kļūdas

--- src/scripts/userClass.ts
+++ src/scripts/userClass.ts
@@ -2,18 +2,21 @@ import { logout, pb } from "./database";
 
 export default class User{
     private name: string|undefined
-    public id: string;
-    constructor(id: string) {
+    public id: string|undefined
+    constructor(id: string|undefined) {
         this.id = id;
     }
     async getName() {
         // ja vārds ir saglabāts, tad atgriež lietotājvārdu
-        if (this.name) {return this.name}
+        if (this.name)
+           return this.name
+        // ja id nav definēts, tas atgriež false
+        if (this.id == undefined)
+           return false
         // atrod vāŗdu pēc lietotāja ID (nākamās 4 rindas)
         let user = await pb
             .collection("lietotaji")
             .getOne(this.id, { $autoCancel: false })
-            .catch(logout)
         // ja lietotājs neeksistē, tad tas atgriež false
         if (!user)
             return false
