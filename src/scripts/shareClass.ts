aad43c68cab82448d3327559230367c4a6fa3874
Davis Martuzans <64132861+DavisMartuzans@users.noreply.github.com>
2023-01-31 18:39:20 +0000

    ShareGame

---
+++ src/scripts/shareClass.ts
@@ -0,0 +1,17 @@
+export class Share {   
+    private spelesID:string
+    constructor(spelesId:string) {
+        this.spelesID=spelesId
+    }
+    public getLink(){
+        return "https://desas.0xff.lv/game/#" + this.spelesID
+    }
+    public copyLink(){
+        console.log(this)
+        const link=this.getLink()
+        navigator.clipboard.writeText(link)
+    }
+}
+
+new Share("spelesId")
+
