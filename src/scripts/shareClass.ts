9dde5ad5c7da66cb0e9133d7a97506ef98b91b23
Edgars <01dtepolis@rvt.lv>
2023-03-21 11:19:25 +0200

    Komentāri

--- src/scripts/shareClass.ts
+++ src/scripts/shareClass.ts
@@ -1,16 +1,21 @@
-export class Share {   
-    private spelesID:string
-    constructor(spelesId:string) {
-        this.spelesID=spelesId
-    }
-    public getLink(){
-        return "https://desas.0xff.lv/game/#" + this.spelesID
-    }
-    public copyLink(){
-        const link=this.getLink()
-        navigator.clipboard.writeText(link)
-    }
+// Definē Share klasi, lai izveidotu koplietošanas saiti un to kopētu uz starpliktuvi
+export class Share {
+  // Privāts spelesID atribūts, kas glabā spēles identifikatoru
+  private spelesID: string;
+  // Konstruktors, kas pieņem spelesId kā argumentu un iestata to kā spelesID atribūtu
+  constructor(spelesId: string) {
+    this.spelesID = spelesId;
+  }
+  // Metode, kas atgriež spēles koplietošanas saiti
+  public getLink() {
+    return "https://desas.0xff.lv/game/#" + this.spelesID;
+  }
+  // Metode, kas kopē spēles koplietošanas saiti uz starpliktuvi
+  public copyLink() {
+    const link = this.getLink();
+    navigator.clipboard.writeText(link);
+  }
 }
 
-new Share("spelesId")
-
+// Izveido jaunu Share objektu ar "spelesId" kā argumentu
+new Share("spelesId");
